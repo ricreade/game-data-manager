6f5a20323483260d7875808ef5d95843422fb734
ricreade <ricreade@gmail.com>
2015-05-16 18:12:38 -0700

    Added communications project for SignalR

---
+++ CommEngine/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+namespace CommEngine
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
