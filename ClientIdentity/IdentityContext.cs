fd05151a497bc750e150cbfcaa1e5621439388e7
ricreade <ricreade@gmail.com>
2015-05-23 23:09:27 -0700

    Added identity project and associated tests project.

---
+++ ClientIdentity/IdentityContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientIdentity
+{
+    /// <summary>
+    /// Encapsulates a user's identity context, including user name,
+    /// access claims, and client information.
+    /// </summary>
+    public class IdentityContext
+    {
+    }
+}
---
+++ ClientIdentityTests/IdentityContextTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClientIdentity;
+
+namespace ClientIdentityTests
+{
+    [TestClass]
+    public class IdentityContextTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
