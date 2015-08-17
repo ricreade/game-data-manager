b49d8354ec5680a111f1c2b7b5242c3a4e3f303f
ricreade <ricreade@gmail.com>
2015-08-16 21:03:55 -0700

    Added dieroll script to handle die rolls.  While this feature is expected to be largely ubiquitous, the details of what it expects may vary, so this functionality has not been incorporated into the main engine.

---
+++ ScriptingEngine/scripts/dieroll.cs
@@ -0,0 +1,16 @@
+﻿using ScriptingEngine;
+
+/// <summary>
+/// This script handles die rolls.  It can be invoked in one of a number
+/// of ways since a die rolls are required in a number of contexts.  The
+/// roll syntax is '{[numdice]d[diesize]} [dmgtype]', repeatable as
+/// necessary.
+/// </summary>
+public class DieRoll : IScriptInstance
+{
+
+    public IScriptResult ProcessRequest(IScriptRequest request)
+    {
+        throw new System.NotImplementedException();
+    }
+}
